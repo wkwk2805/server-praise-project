e79e328916bb49309c68f51d709d8e36ab097e6a
wkwk2805 <wkwk2805@gmail.com>
2019-06-23 22:17:23 +0900

    make insert, update, delete api

--- app.js
+++ app.js
@@ -3,20 +3,43 @@ const app = express();
 const bodyParser = require("body-parser");
 const low = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
+const adapter = new FileSync("lyrics.json");
+const db = low(adapter);
 
-//db.defaults({ posts: [], user: {} }).write();
-
-app.get("/create_db", (req, res) => {
-  const adapter = new FileSync("db.json");
-  const db = low(adapter);
-  db.defaults({ lyrics: [1, 2, 3] }).write();
-  res.send("create File");
-});
+//body-parser setting
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello express");
+//create database schema
+app.get("/create_db", (req, res) => {
+  db.defaults({ lyrics: [] }).write();
+  res.send("create File");
+});
+
+//insert data
+app.put("/api/insert", (req, res) => {
+  db.get("lyrics")
+    .push(req.body)
+    .write();
+  res.json({ result: "success", message: "등록성공" });
+});
+
+//update data
+app.patch("/api/update", (req, res) => {
+  db.get("lyrics")
+    .find({ id: req.body.id })
+    .assign(req.body)
+    .write();
+  res.json({ result: "success", message: "수정성공" });
+});
+
+//select data
+app.post("/api/select", (req, res) => {});
+
+//delete data
+app.delete("/api/delete", (req, res) => {
+  db.get("lyrics").remove({ id: req.body.id });
+  res.json({ result: "success", message: "삭제성공" });
 });
 
 app.listen(3001, () => {
